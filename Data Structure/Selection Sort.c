c396e7fbeb7f88de711e1487b7f4fce64469c22d
Md. Fartin Mahtadi Fuad <111303366+fmfuad0@users.noreply.github.com>
2022-11-16 15:08:10 +0600

    Add files via upload

---
+++ Data Structure/Selection Sort.c
@@ -0,0 +1,33 @@
+
+///Selection Sort
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int arr[10];
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    int t=10;
+    int temp,index;
+    for(int i=0;i<10;i++)
+    {
+        int max=arr[0];
+        for(int j=0;j<10-i;j++)
+        {
+            if(arr[j]>=max)
+            {
+                max=arr[j];
+                index=j;
+            }
+        }
+        temp=arr[10-1-i];
+        arr[10-1-i]=max;
+        arr[index]=temp;
+    }
+    printf("\n\nSORTED ARRAY: \n");
+    for(int i=0;i<10;i++)
+        printf("%d ",arr[i]);
+}
